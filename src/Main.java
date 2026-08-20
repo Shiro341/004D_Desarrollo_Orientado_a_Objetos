41e2f40df12e0858e19a9fc9cdecb735edba8f2d
Shiro341 <tecnicocode@proton.me>
2026-08-20 17:13:55 -0400

    clase 4

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,65 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        showMenu();
-        
+       mostrarMenu();
+
     }
-
-    static void showMenu(){
-        Personaje personaje_player = new Personaje();
-        String opcion_elegida;
-        Scanner scanner = new Scanner(System.in); //toda esta linea es una instancia
-
-        System.out.println("**Bienvendios al mundo de Westeros**");
-        boolean start_menu = true;
-
+    
+    static void mostrarMenu(){
+        String opcion_menu;
+        boolean mantener_menu = true;
         
-        while (start_menu) {
+        Personaje personaje_jugador = new Personaje();
+
+        Scanner scanner = new Scanner(System.in); 
+
+        while (mantener_menu) {
             System.out.println("1.- Crear personaje");
             System.out.println("2.- Ver personaje");
-            System.out.println("3.- Salir");
+            System.out.println("3.- Cerrar");
+            System.out.print("Elija su opción: ");
+            opcion_menu = scanner.nextLine();
 
-            System.out.print("Por favor ingrese una opción: ");
-            opcion_elegida = scanner.nextLine();
-
-            System.out.println("La opción escogida por el usuario es: " + opcion_elegida);
-
-
-            //Este sirve para hacer acciones en función de un valor o sus posibles valaores
-        
-            switch (opcion_elegida) {
+            switch (opcion_menu) {
                 case "1":
-                    System.out.println("Creación de personaje");
-                    
+                    System.out.println("Creación del Personaje");
+                    System.out.println("¿Cómo llamaras al personaje?");
+                    personaje_jugador.setNombre( scanner.nextLine());
+                    System.out.println("¿Cómo llamaras al apellido del personaje?");
+                    personaje_jugador.apellido = scanner.nextLine();
+                    System.out.println("Asigna la vida del personaje");
+                    //personaje_jugador.vida = Integer.parseInt(scanner.nextLine());
+                    personaje_jugador.setVida(Integer.parseInt(scanner.nextLine()));
+                    //Pero haber creado el metodo no es suficiente, por que si el atributo sigue publico, el encapsulamiento no tiene sentido. Revisemos con un ejemplo, observa la siguiente linea
+                    //personaje_jugador.vida = -20;
+                    //Para que esto no sea posible, declararemos el atributo como privado
+                    System.out.println("¿Cúal es la edad del jugador?");
+                    personaje_jugador.setEdad(Integer.parseInt(scanner.nextLine()));
+                    break;
+                case "2":
+                    System.out.println();
+                    System.out.println("Datos del personaje creado");
+                    System.out.println("Nombre personaje: " + personaje_jugador.getNombre());
+                    System.out.println("Apellido personaje: " + personaje_jugador.apellido);
 
-                    System.out.println("¿Cómo llamaras a tu héroe?: ");
-                    personaje_player.name = scanner.nextLine();
-                    System.out.println("¿what is the playe´s name?: " + personaje_player.name);
+                    //OJITO CON ESTO, ESTO ES LA LUZ!
+                    //REFLEXIONA, POR QUÉ ES MEJOR ESTO?
+                    //ENCAPSULANDO
+                    System.out.println("Vida personaje: " + personaje_jugador.getVida());
+                    System.out.println("¿Se encuentra vivo?: " + personaje_jugador.esta_vivo);
 
-                    System.out.println("¿Cuál es el apellido del héroé?: ");
-                    personaje_player.lastName = scanner.nextLine();
-                    System.out.println("¿Cúal es el apellido del jugador?: " + personaje_player.lastName);
-
-                    System.out.println("Indique la vida del héroe: ");
-                    // Ya no lo haremos así, por que es inseguro
-                    //personaje_player.life = Integer.parseInt(scanner.nextLine());
-
-                    personaje_player.setearVida(Integer.parseInt(scanner.nextLine()));
-
+                    System.out.println("Edad: " + personaje_jugador.getEdad());
+                    break;
+                case "3":
+                    System.out.println("Cosas de opcion 3");
+                    mantener_menu = false;
                     break;
             
-                case "2":
-
-                    System.out.println("Ver Personaje");
-                    System.out.println("Nombre: " + personaje_player.name);
-                    System.out.println("Apellido: " + personaje_player.lastName);
-                    System.out.println("Vida: " + verVida());
-                    
-                    break;
-
-                case "3":
-                    System.out.println("Gracias por jugar");
-                    start_menu = false;
-                    break;
-        
                 default:
-                    System.out.println("Por favor ingrese una de las opciones permitidas");
+                    System.out.println("Por favor ingrese una opción valida");
                     break;
-        
             }
         }
     }
--- src/Personaje.java
+++ src/Personaje.java
@@ -1,20 +1,68 @@
 public class Personaje {
-    String name;
-    String lastName;
-    private int life;
-    boolean isHeAlive = true;
+    private String nombre;
+    String apellido;
+    private int vida;
+    boolean esta_vivo = true;
+    private int edad;
 
-    public void setearVida(int life){
-
-        if (life < 0){
-            this.life = 0;
-        } else{
-            this.life = life;
+    public void setNombre(String nombre){
+        switch (nombre) {
+            case "tonto":
+                System.out.println("No puedes colocar un insulto como un nombre");
+                break;
+            case "sopenco":
+                System.out.println("No puedes colocar un insulto como un nombre");
+                break;
+            case "conchudo":
+                System.out.println("No puedes colocar un insulto como un nombre");
+                break;
+            case "weon":
+                System.out.println("No puedes colocar un insulto como un nombre");
+                break;
+            case "pipi":
+                System.out.println("No puedes colocar un insulto como un nombre");
+                break;
+            case "caca":
+                System.out.println("No puedes colocar un insulto como un nombre");
+                break;
+        
+            default:
+                break;
         }
-
     }
 
-    public int verVida(){
-        return this.life;
+    public String getNombre(){
+        return this.nombre;
     }
+
+
+    public void setVida(int vida){ //esto quiere guardar la vida no darla
+        if (vida < 0){
+            System.out.print("La vida del personaje no puede ser negativa. Que demonios haces!");
+            this.vida = 0;
+        } else{
+            this.vida = vida;
+        }
+    }
+
+    public int getVida(){
+        return this.vida;
+    }
+
+    public void setEdad(int edad){
+        if (vida < 0){
+            System.out.println("La edad no puede ser negativa, se le asigna la edad de 18 por defecto");
+            this.edad = 18;
+        }
+        else{
+            this.edad = edad;
+        }
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+
+
 }
+
